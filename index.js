4ab4dd875c1633718ceca9aa335c33b118dc1bc1
danyela822 <danyela822@hotmail.com>
2022-06-02 03:08:44 -0500

    Primera version del timer

--- index.js
+++ index.js
@@ -4,8 +4,10 @@
 //     clearInterval(myInterval);
 // }, 3000);
 
-//valor actual del cronometro
-let currentChronometer
+let section = document.querySelector("#section");
+
+//valor actual del intervalo
+let currentInterval;
 
 //span que muestra los segundos en pantalla
 let secondsSpan = document.querySelector('#seconds');
@@ -17,10 +19,19 @@ let secondsValue = 0;
 //valor de los minutos
 let minutesValue = 0;
 
+let currentButton;
+
+let timerButton = document.querySelector("#timer-button");
+
 function starChronometer(){
 
+    //Donde estamos haciendo click
+    currentButton = event.target;
+    //Desactivar boton
+    currentButton.disabled = true;
+
     //Esta funcion anonima solo vive dentro de la funcion setInterval la cual ejecuta dicha funcion por un cierto tiempo
-    currentChronometer = setInterval(() => {
+    currentInterval = setInterval(() => {
         console.log("Ha pasado un segundo")
 
         if(secondsValue === 59){
@@ -30,7 +41,7 @@ function starChronometer(){
         }
         secondsValue += 1;
         secondsSpan.textContent = formatValue(secondsValue);
-    },1000);
+    },100);
 }
 
 function formatValue(value){
@@ -43,7 +54,11 @@ function formatValue(value){
 }
 
 function stopChronometer() {     
-    clearInterval(currentChronometer);
+    clearInterval(currentInterval);
+    
+    if(currentButton){
+        currentButton.disabled = false;
+    }
 }
 
 function resetChronometer(){
@@ -51,4 +66,44 @@ function resetChronometer(){
     minutesValue = 0;
     secondsSpan.textContent = "00";
     minutesSpan.textContent = "00";
+}
+
+function exucuteTimer(){
+    
+    section.innerHTML = 
+        `<h1 class="main-section__title" id="title">Timer</h1>
+        <p class="main-section__time"><span id="minutes">00</span>:<span id="seconds">00</span></p>
+        <div class="main-section__buttons-box">
+            <form onsubmit="startTimer()">
+                <input type="number" placeholder="Escribe los minutos" id="minutes-input" name="minutes">
+                <input type="number" placeholder="Escribe los segundos" id="seconds-input" name="seconds">
+                <button type="submit" class="main-section__buttons">Start</button>
+            </form>
+        </div>`
+
+    secondsSpan = document.querySelector('#seconds');
+    minutesSpan = document.querySelector('#minutes');
+}
+function startTimer(){
+    event.preventDefault();
+    const minutes = parseInt(event.target.minutes.value);
+    const seconds = parseInt(event.target.seconds.value);
+
+    minutesSpan.textContent = minutes;
+    secondsSpan.textContent = seconds;
+    minutesValue = minutes;
+    secondsValue = seconds;
+
+    currentInterval = setInterval(() => {
+        secondsValue -= 1;
+        if(secondsValue === -1){
+            secondsValue = 59;
+            minutes -= 1;
+        }
+        if(minutesValue === 0 && secondsValue === 0){
+            clearInterval(currentInterval);
+        }
+        minutesSpan.textContent = formatValue(minutesValue);
+        secondsSpan.textContent = formatValue(secondsValue);
+    },1000)
 }
